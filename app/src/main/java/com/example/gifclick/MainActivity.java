bb3eac620935db003c613ec749e56c763143213e
Farhad <imfarhad01@gmail.com>
2020-07-15 13:07:40 +0600

    done 1

--- app/src/main/java/com/example/gifclick/MainActivity.java
+++ app/src/main/java/com/example/gifclick/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.gifclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -19,7 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
     GifImageView gifImageView;
     ImageView imageView;
-    Button button;
+    Button button, btnReset, btn33, btn99, btnttlr;
+    TextView tvCurrentCounter, tvSetCount, tvTotalcount;
+
+
+
+    int currentcounter =0;
+    int countset = 33;
+    int totalcount =0;
+    String defaultValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +38,122 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.idImage);
         gifImageView = findViewById(R.id.idGif);
         button = findViewById(R.id.idButton);
+        btnReset = findViewById(R.id.idReset);
+        tvCurrentCounter = findViewById(R.id.idCurrentCount);
+        btn33 = findViewById(R.id.id33);
+        btn99 = findViewById(R.id.id99);
+        tvSetCount = findViewById(R.id.idCountSet);
+        tvTotalcount = findViewById(R.id.idtotalCount);
+        btnttlr = findViewById(R.id.idttlr);
+
+        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        currentcounter = sharedPref.getInt("Count",0);
+        countset = sharedPref.getInt("CountSet",0);
+        totalcount = sharedPref.getInt("TotalCount",0);
+        tvCurrentCounter.setText(""+ currentcounter);
+        tvTotalcount.setText(""+ totalcount);
+
+        btn33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvSetCount.setText("33");
+
+                countset = 33;
+                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("CountSet", Integer.parseInt(""+ countset));
+                editor.commit();
+            }
+        });
+
+        btn99.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvSetCount.setText("99");
+                countset = 99;
+                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("CountSet", Integer.parseInt(""+ countset));
+                editor.commit();
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+//                MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.tosbiscountsound);
+//                mediaPlayer.start();
+
+                if (currentcounter == countset){
+                    currentcounter = 0;
+                }
+
+                currentcounter = currentcounter +1;
+                totalcount = totalcount +1;
+
+
+
+
+                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("Count", Integer.parseInt(""+ currentcounter));
+                editor.putInt("TotalCount", Integer.parseInt(""+ totalcount));
+                editor.commit();
+
+                //defaultValue = sharedPref.getString("LastSuraName","");
+
+                tvCurrentCounter.setText(""+ currentcounter);
+                tvTotalcount.setText(""+totalcount);
+
 
 
               thread();
+
+            }
+        });
+
+
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                currentcounter = 00;
+
+                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("Count", Integer.parseInt(""+ currentcounter));
+                editor.commit();
+
+                //defaultValue = sharedPref.getString("LastSuraName","");
+
+                tvCurrentCounter.setText(""+ currentcounter);
+
+
+            }
+        });
+
+        btnttlr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                currentcounter = 00;
+                totalcount = 00;
+
+
+                SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("Count", Integer.parseInt(""+ currentcounter));
+                editor.putInt("TTlCount", Integer.parseInt(""+ totalcount));
+                editor.commit();
+
+                //defaultValue = sharedPref.getString("LastSuraName","");
+
+                tvCurrentCounter.setText(""+ currentcounter);
+                tvTotalcount.setText(""+ totalcount);
+
 
             }
         });
@@ -63,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
-        }, 900);
+        }, 500);
 
     }
 
